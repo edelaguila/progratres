0b9a10c8749b2958b9b9f5dd9d03fb3dc1a1bf6f
Esduardo Del Aguila <daguilae@yahoo.com>
2022-02-03 11:50:52 -0600

    Creacion del modelo BD

--- ProyectoCapas/scripts/Script Prueba.sql
+++ ProyectoCapas/scripts/Script Prueba.sql
@@ -1,3 +1,4 @@
+DROP DATABASE prueba;
 CREATE DATABASE prueba;
 USE prueba;
 CREATE TABLE usuario(
@@ -7,15 +8,20 @@ CREATE TABLE usuario(
     usutelefono VARCHAR(30) NOT NULL,
     usucorreo VARCHAR(60) NOT NULL,
 	usucontra VARCHAR(100) NOT NULL,
-	estado VARCHAR(1) NOT NULL	
+	usuestado VARCHAR(1) NOT NULL	
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-CREATE TABLE usuario(
-	usucodigo VARCHAR(15) PRIMARY KEY,
-	usunombre varchar(60) NOT NULL, 
-	usudireccion VARCHAR(60) NOT NULL,
-    usutelefono VARCHAR(30) NOT NULL,
-    usucorreo VARCHAR(60) NOT NULL,
-	usucontra VARCHAR(100) NOT NULL,
-	estado VARCHAR(1) NOT NULL,
-	FOREIGN KEY (fkIdEmpleado) REFERENCES empleado(pkIdEmpleado)
+CREATE TABLE aplicaciones(
+	aplcodigo VARCHAR(5) PRIMARY KEY,
+	aplnombre varchar(60) NOT NULL, 
+	aplestado VARCHAR(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE bitacora (
+	bitcodigo int auto_increment PRIMARY KEY,
+    bitfecha datetime NULL, 
+	bitaccion VARCHAR(10) NOT NULL,
+    usucodigo VARCHAR(15) NOT NULL,
+    aplcodigo VARCHAR(5) NOT NULL,
+	FOREIGN KEY (usucodigo) REFERENCES usuario (usucodigo),
+    FOREIGN KEY (aplcodigo) REFERENCES aplicaciones (aplcodigo)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
